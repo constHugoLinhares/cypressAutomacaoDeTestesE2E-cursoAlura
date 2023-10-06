ce90dba79a642ee98331a03e3522edb09a279a7f
constHugoLinhares <hugolinhares92@gmail.com>
2023-10-06 16:54:15 -0300

    módulo 5, requisições com API REST e flaky testes

--- cypress/e2e/api-alurapic.cy.js
+++ cypress/e2e/api-alurapic.cy.js
@@ -1,14 +1,16 @@
 describe(() => {
-    it('Dados da API',() => {
+    it('Fotos do Usuário',() => {
+        const tempoEsperado = Math.random() * 50
+
         cy.request({
             method: 'POST',
-            url: 'http://localhost:3000/user/login',
-            body: Cypress.env()
+            url: 'http://localhost:3000/hugop/photos'
         }).then((res) => {
             expect(res.status).to.be.equal(200);
             expect(res.body).is.not.empty
-            expect(res.body).to.have.property('id')
-            expect(res.body.id).to.be.equal(4);
+            expect(res.body[0]).to.have.property('description')
+            expect(res.body[0].description).to.be.equal('porquinho');
+            expect(res.duration).to.be.lte(tempoEsperado);
         })
     })
 })
